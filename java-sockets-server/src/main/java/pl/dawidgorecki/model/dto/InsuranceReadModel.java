0b5a78e91affdfbe8ff3b6f6bb6277cd75512d1d
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2023-02-09 14:53:31 +0100

    DTO (projection)

---
+++ java-sockets-server/src/main/java/pl/dawidgorecki/model/dto/InsuranceReadModel.java
@@ -0,0 +1,22 @@
+package pl.dawidgorecki.model.dto;
+
+import lombok.Getter;
+import pl.dawidgorecki.model.Insurance;
+
+import java.math.BigDecimal;
+
+@Getter
+public class InsuranceReadModel {
+    private final String insurer;
+    private final BigDecimal price;
+
+    public InsuranceReadModel(Insurance insurance) {
+        insurer = insurance.getInsurer();
+        price = insurance.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return "Ubezpieczyciel: " + insurer + ", cena: " + price;
+    }
+}
---
+++ java-sockets-server/src/main/java/pl/dawidgorecki/model/dto/VehicleReadModel.java
@@ -0,0 +1,28 @@
+package pl.dawidgorecki.model.dto;
+
+import lombok.Getter;
+import pl.dawidgorecki.model.Vehicle;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class VehicleReadModel {
+    private final String brand;
+    private final String model;
+    private final List<InsuranceReadModel> insuranceOffers;
+
+    public VehicleReadModel(Vehicle vehicle) {
+        brand = vehicle.getBrand();
+        model = vehicle.getModel();
+
+        insuranceOffers = vehicle.getInsuranceOffers().stream()
+                .map(InsuranceReadModel::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return "Marka:  " + brand + ", model: " + model + ", oferty: " + insuranceOffers;
+    }
+}
